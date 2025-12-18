099d789cec9b9eb680ed3c83aa18f4cfdeb5814a
unknown <sandesh4080@gmail.com>
2025-12-18 11:59:04 +0530

    rrtos_F00 re generate from ioc

--- RTOS_F00/Core/Src/main.c
+++ RTOS_F00/Core/Src/main.c
@@ -271,20 +271,21 @@ void Error_Handler(void) {
 }
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
-void assert_failed(uint8_t *file, uint32_t line) {
-	/* USER CODE BEGIN 6 */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
 	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 	SEGGER_SYSVIEW_PrintfHost("Assertion Failed:file %s on line %d\r\n", file,
 			line);
 	while (1)
 		;
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
